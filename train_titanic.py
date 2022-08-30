67c28ddfac528328a9751d17877299db78703b78
euiraekim <harrykur139@gmail.com>
2022-08-30 13:39:37 +0900

    dag file 수정

--- train_titanic.py
+++ train_titanic.py
@@ -22,11 +22,51 @@ download_dataset = BashOperator(
 )
 
 def _preprocessing_data():
-    print('preprepredata')
+    df = pd.read_csv('/tmp/titanic.csv')
+
+    # 결측값 채우기
+    df['Age'].fillna(df['Age'].mean(), inplace=True)
+    df['Cabin'].fillna('N', inplace=True)
+    df['Embarked'].fillna('N', inplace=True)
+
+    # 컬럼 제거
+    df.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+
+    # lable encoding
+    df['Cabin'] = df['Cabin'].str[:1]
+    features = ['Cabin', 'Sex', 'Embarked']
+    for feature in features:
+        le = LabelEncoder()
+        le = le.fit(df[feature])
+        df[feature] = le.transform(df[feature])
+
+    df.to_csv('/tmp/titanic_preprocessed.csv', index=False)
+
+def _train_data():
+    df = pd.read_csv('/tmp/titanic_preprocessed.csv')
+
+    # x, y split
+    y_df = titanic_df['Survived']
+    X_df = titanic_df.drop('Survived', axis=1)
+
+    # train test split
+    X_train, X_test, y_train, y_test = train_test_split(X_df, y_df, test_size=0.2, random_state=11)
+
+    # train
+    lr_clf = LogisticRegression()
+    lr_clf.fit(X_train, y_train)
+    lr_pred = lr_clf.predict(X_test)
+    print('LogisticRegression 정확도: {0:.4f}'.format(accuracy_score(y_test, lr_pred)))
 
 preprocessing_data = PythonOperator(
     task_id='preprocessing_data',
     python_callable=_preprocessing_data,
+    dag=dag
+)
+
+train_data = PythonOperator(
+    task_id='train_data',
+    python_callable=_train_data,
     dag=dag
 )
 
